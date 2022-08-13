303c2ae4e67c86467f874de392d2749a4330797d
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2022-08-13 13:47:22 +0530

    fix camera again 4

--- src/components/Fifth.jsx
+++ src/components/Fifth.jsx
@@ -120,16 +120,16 @@ const Fifth = () => {
                 style={{
                   position: "absolute",
                   top: 0,
-                  height: 320,
-                  width: 320,
+                  height: 330,
+                  width: 330,
                 }}
               />
               <img
                 src={frame}
                 alt="captured-img"
                 style={{
-                  width: 320,
-                  height: 320,
+                  width: 330,
+                  height: 330,
                   zIndex: 2,
                   position: "absolute",
                   top: 0,
